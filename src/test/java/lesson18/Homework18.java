f77723fdd8cc12ad6aa0242b6458d820f23ecf01
DaymianDark <nightkonn@gmail.com>
2022-03-14 16:47:47 +0300

    try hw 18

--- src/test/java/lesson18/Homework18.java
+++ src/test/java/lesson18/Homework18.java
@@ -1,7 +1,11 @@
 package lesson18;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -11,37 +15,37 @@ public class Homework18 {
     void addToCartTest() {
 
         /*
-curl "http://demowebshop.tricentis.com/addproducttocart/details/31/1" ^
-  -H "Connection: keep-alive" ^
-  -H "Accept: *" ^
-                -H "DNT: 1" ^
-                -H "X-Requested-With: XMLHttpRequest" ^
-                -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36" ^
-                -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" ^
-                -H "Origin: http://demowebshop.tricentis.com" ^
-                -H "Referer: http://demowebshop.tricentis.com/141-inch-laptop" ^
-                -H "Accept-Language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6" ^
-                -H "Cookie: Nop.customer=0a69af50-b3c3-4b38-9944-dc7f1ca17377; __utmz=78382081.1647077729.1.1.utmcsr=(direct)^|utmccn=(direct)^|utmcmd=(none); ARRAffinity=a1e87db3fa424e3b31370c78def315779c40ca259e77568bef2bb9544f63134e; __utmc=78382081; __utma=78382081.503175620.1647077729.1647171467.1647176957.6; NopCommerce.RecentlyViewedProducts=RecentlyViewedProductIds=31&RecentlyViewedProductIds=72&RecentlyViewedProductIds=2&RecentlyViewedProductIds=16; __atuvc=19^%^7C10^%^2C5^%^7C11; __atuvs=622df1aac033815a002; __utmb=78382081.8.10.1647176957" ^
-                --data-raw "addtocart_31.EnteredQuantity=1" ^
-                --compressed ^
-                --insecure
+
          */
 
-        Integer cartSize = 0;
+        String addToCartCookie =
+                given()
+                        .contentType("application/x-www-form-urlencoded;")
+                        .body("addtocart_31.EnteredQuantity=1")
+                        .when()
+                        .post("http://demowebshop.tricentis.com/addproducttocart/details/31/1")
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .extract()
+                        .cookie("ADDTOCART.COOKIE");
 
-        given()
-                .contentType("application/x-www-form-urlencoded;")
-                .cookie("Nop.customer=0a69af50-b3c3-4b38-9944-dc7f1ca17377;")
-                .body("addtocart_31.EnteredQuantity=1")
-                .when()
-                .post("http://demowebshop.tricentis.com/addproducttocart/details/31/1")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .body("success", is(true))
-                .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
-                .body("updatetopcartsectionhtml", is("(" + cartSize + ")"));
+        open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
+        getWebDriver().manage().addCookie(new Cookie("ADDTOCART.COOKIE", addToCartCookie));
 
+        open("http://demowebshop.tricentis.com/cart");
+        getWebDriver().manage().addCookie(new Cookie("ADDTOCART.COOKIE", addToCartCookie));
+
+        $(".cart").shouldHave(text("14.1-inch Laptop"));
+        closeWebDriver();
     }
+
+    @Test
+    void hzTest() {
+        open("http://demowebshop.tricentis.com/cart");
+        $(".cart").shouldHave(text("14.1-inch Laptop"));
+        closeWebDriver();
+    }
+
 
 }
--- src/test/java/lesson18/demoWebShopAddToCart.java
+++ src/test/java/lesson18/demoWebShopAddToCart.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.shouldHaveThrown;
 import static org.hamcrest.CoreMatchers.is;
 
 public class demoWebShopAddToCart {
@@ -50,6 +51,32 @@ public class demoWebShopAddToCart {
                 .body("success", is(true))
                 .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
                 .body("updatetopcartsectionhtml", is("(" + cartSize + ")"));
+
+//        assertThat(response.extract().path("updatetopcartsectionhtml").toString())
+//                .body("updatetopcartsectionhtml", is("(" + cartSize + ")");
+    }
+
+    @Test
+    void addToCartWithCookieToStringTest() {
+
+        String response =
+                given()
+                        .contentType("application/x-www-form-urlencoded;")
+                        .cookie("Nop.customer=0a69af50-b3c3-4b38-9944-dc7f1ca17377;")
+                        .body("product_attribute_72_5_18=53" +
+                                "&product_attribute_72_6_19=54" +
+                                "&product_attribute_72_3_20=57" +
+                                "&addtocart_72.EnteredQuantity=1")
+                        .when()
+                        .post("http://demowebshop.tricentis.com/addproducttocart/details/72/1")
+                        .then()
+//                        .log().all()
+                        .statusCode(200)
+                        .body("success", is(true))
+                        .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
+//                        .body("updatetopcartsectionhtml", is("(37)"))
+                        .extract().path("updatetopcartsectionhtml").toString();
+        System.out.println(response);
 
 //        assertThat(response.extract().path("updatetopcartsectionhtml").toString())
 //                .body("updatetopcartsectionhtml", is("(" + cartSize + ")");
